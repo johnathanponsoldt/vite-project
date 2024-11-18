6bf3ef26ed14e2982a1ac8400542fb61a2528fd3
johnathanponsoldt <jponsoldt772@gmail.com>
2024-11-17 21:51:56 -0500

    Create AddProductForm component with form fields

---
+++ src/AddProductForm.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const AddProductForm = ({ addProduct })
+const [formData, setFormData] = useState({
+    id: 3,
+    name: "Volleyball",
+    price: "20",
+    description: "High Quality Volleyballs",
+  });
+  
